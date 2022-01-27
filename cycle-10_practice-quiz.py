2ee7f90ff3d066276fb8d65332a9e4bd22fdd7f3
p22mbonilla <p22mbonilla@fairfieldprep.org>
2022-01-27 10:20:44 -0500

    addfiler

---
+++ cycle-10_practice-quiz.py
@@ -0,0 +1,11 @@
+# Auhor MB 01/27/2022
+
+#1
+def food_costs(groceries,costs):
+    groceries_mod = groceries
+    index = 0
+        if i, v in enumerate(groceries_mod):
+            
+
+    return groceries_mod
+print(food_costs(['apple','pear','banana',],['salmon','tuna','cod',],['milk','eggs','yogurt',],[1.99,2.99,0.99,9.99,10.99,6.99,3.49,2.49,1.49]) == ['apple: $1.99','pear: $2.99','banana: $0.99',],['salmon: $9.99','tuna: $10.99','cod:$6.99',],['milk: $3.49','eggs: $2.49','yogurt: $1.49',])
